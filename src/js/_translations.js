57036d7cf2b77d4779b589b7aad9519214bf727c
C15514A <emilie.rochedy@experian.com>
2017-02-06 11:38:23 +0000

    Update _translations.js
    Added FR translation

--- src/js/_translations.js
+++ src/js/_translations.js
@@ -14,4 +14,16 @@ ContactDataServices.translations = {
     }
   }
   // Add other languages below
+  fr: {    
+    FRA: {      
+      locality: "Ville",      
+      province: "Departement",      
+      postalCode: "Code Postal"    
+          },
+     FRX: {      
+        locality: "Ville",      
+        province: "Departement",      
+        postalCode: "Code Postal"    
+          }
+       }
 };
